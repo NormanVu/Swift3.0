fa64bb553806619a985ff830731ad07bb0a441cf
Norman <norman@enclave.vn>
2017-10-10 16:43:40 +0700

    Update section 1 and 2

--- Day1/1.1/HelloWorld.playground/Contents.swift
+++ Day1/1.1/HelloWorld.playground/Contents.swift
@@ -7,6 +7,44 @@ let ell = "ell"
 let o: String? = "o"
 var world: String! = nil
 world = " world!"
+var helloWorld: String
 
-let helloWorld: String = "Hello world!"
+
+//Combination two strings by plus when print
+print ( " Hello "  +  " World! " )
+
+//Combination strings
+if let h2 = h, let o2 = o, let world2 = world {
+    helloWorld = h2 + ell + o2 + world2
+} else {
+    helloWorld = ""
+}
 print(helloWorld)
+
+//Combination string and characters
+let a: String = " World! Police number: "
+var b: Int = 911
+print (" Hello " + String(a) + String(b));
+
+
+
+//-------------------------------------------------------
+//Task: Display "Hello World!" without using force unwrap
+//-------------------------------------------------------
+
+//------------------
+//Using force unwrap: However, if you use force unwrap, if the variable is nil, it will be a run-time error when print value, so do not to try to use it much
+var forceUnwrap : String? = nil
+forceUnwrap = "Hello World!"
+print(forceUnwrap!)
+
+//--------------------------
+//Without using force unwrap
+//Solution 1:
+var withoutForceUnwrap : String! = nil
+withoutForceUnwrap = "Hello World!"
+print(withoutForceUnwrap)
+//Solution 2:
+//var forceUnwrap : String? = nil
+let newWithoutForceUnwrap = forceUnwrap ?? ""
+print(newWithoutForceUnwrap)
--- Day1/1.2/Leak.playground/Contents.swift
+++ Day1/1.2/Leak.playground/Contents.swift
@@ -2,4 +2,45 @@
 
 import UIKit
 
-var str = "Hello, playground"
+//----------------------------------------------------------------------------------------------------
+//Task: Let's actually see how the reference flow changes with strong reference and weak reference
+//----------------------------------------------------------------------------------------------------
+
+class ParentObject {
+    //Default strong reference
+    var object: AnyObject?
+
+    deinit {
+        print("ParentObject: released")
+    }
+}
+
+class ChildObject {
+    //Weak reference can prevent circular reference
+    weak var object: AnyObject?
+
+    deinit {
+        print("ChildObject: released")
+    }
+}
+
+
+// parentObj possesses a strong reference to an instance of ParentObject
+var parentObj : ParentObject?  =  ParentObject()
+
+// childObj possesses a strong reference to an instance of ChildObject
+var childObj : ChildObject?  =  ChildObject()
+
+// instance variable of parentObj holds a strong reference to childObj
+parentObj?.object  = childObj
+// ChildObj instance variable of holding strong references to the parentObj
+childObj?.object  = parentObj
+
+// If parentObj.object is a weak reference:
+// Substituting nil for parentObj will cause parentObj to dereference (release) an instance of ParentObject
+// childObj will be nil so childObj will release an instance of ChildObject
+parentObj =  nil
+
+// if parentObj.object is a weak reference:
+// Substituting nil for parentObj, childObj.object holds strong reference of instance of ParentObject
+parentObj =  nil
