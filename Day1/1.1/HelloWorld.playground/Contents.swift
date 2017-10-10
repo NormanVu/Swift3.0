ea6b1e4535905e3c637343ea6c6a444dba42724e
Norman <norman@enclave.vn>
2017-10-10 13:48:25 +0700

    Add section 1.1

---
+++ Day1/1.1/HelloWorld.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let h: String? = "H"
+let ell = "ell"
+let o: String? = "o"
+var world: String! = nil
+world = " world!"
+
+let helloWorld: String = "Hello world!"
+print(helloWorld)
