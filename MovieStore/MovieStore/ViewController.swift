117d0e74e91a479a25ac4350af562b9e479f5868
Norman <norman@enclave.vn>
2017-10-17 14:38:20 +0700

    Add initialize project

---
+++ MovieStore/MovieStore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieStore
+//
+//  Created by Nhat (Norman) H.M. VU on 10/17/17.
+//  Copyright © 2017 enclaveit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
